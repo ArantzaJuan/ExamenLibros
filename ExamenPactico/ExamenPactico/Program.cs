018c967abf6ead5aba2c3e6f321bfffd71ddb06a
ArantzaJuan <arantzajuanal@gmail.com>
2022-12-09 16:17:46 -0600

    SQL

---
+++ ExamenPactico/ExamenPactico/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenPactico
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Limite;
+            Console.WriteLine("Ingrese un numero limite para realizar la serie de Fibonacci");
+            Limite =int.Parse( Console.ReadLine());
+            Console.WriteLine("**************************");
+            int resul=1;
+            for (int i = 0; i < Limite; i++)
+            {
+                
+                
+            
+                Console.WriteLine(resul);
+
+            }
+
+        }
+    }
+}
